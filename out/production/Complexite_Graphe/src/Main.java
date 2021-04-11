d335fe7a70569fcd1fa96b40a3af50081fb50c71
Yaizlll <56202553+Yaizlll@users.noreply.github.com>
2021-04-11 20:28:35 +0200

    version_1.0
    fs 始终为0

--- out/production/Complexite_Graphe/src/Main.java
+++ out/production/Complexite_Graphe/src/Main.java
@@ -6,13 +6,19 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Graphe g = new RandomGraphe(15,32);
+        Graphe g = new RandomGraphe(10,32);
        // g.export();
         System.out.println(g.toString());
+          //System.out.println(g.MethodeSeq());
+
+        GraphADJ g_adj = new GraphADJ(g);
+       // System.out.println (g_adj.heuristiqueControle());
+        System.out.println(g_adj.TabuSearch());
     }
 }
